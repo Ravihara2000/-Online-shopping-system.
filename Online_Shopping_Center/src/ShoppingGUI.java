29ae4d3e46cd8eb1b7a7a7464b780f7c8d2c6118
Ravihara De Silva <117923473+Ravihara2000@users.noreply.github.com>
2023-11-13 16:56:04 +0530

    display the selected product details method done

--- Online_Shopping_Center/src/ShoppingGUI.java
+++ Online_Shopping_Center/src/ShoppingGUI.java
@@ -4,16 +4,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.awt.Color;
 
 public class ShoppingGUI extends JFrame {
     private DefaultTableModel tableModel;
+    private JPanel productDetailsPanel;
     public ShoppingGUI(WestminsterShoppingManager manager) {
         JFrame f1 = new JFrame();
         f1.setSize(700, 400);
         f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f1.setLayout(new BorderLayout());
         f1.setTitle("Westminster Shopping Center");
+        f1.getContentPane().setBackground(new Color(255,100,55));
         f1.setLocationRelativeTo(null); // GUI comes to the middle of the screen
 
         JLabel titleLabel = new JLabel("Westminster Shopping Center");
@@ -66,22 +70,43 @@ public class ShoppingGUI extends JFrame {
         f1.add("Center", panel);
 
 
-        String[] columnNames = {"Product ID", "Name", "Category", "Price", "Info"};
+        String[] columnNames = {"Product ID", "Name", "Category", "Price","Number of Items", "Info"};
 // Remove the DefaultTableModel type declaration before tableModel
         tableModel = new DefaultTableModel(columnNames, 0);
         JTable productListTable = new JTable(tableModel);
 
+        // Create a panel for product details
+        productDetailsPanel = new JPanel();
+        productDetailsPanel.setLayout(new BoxLayout(productDetailsPanel, BoxLayout.Y_AXIS));
+        f1.add("Center", productDetailsPanel);
+
+        // Add a ListSelectionListener to the table
+        productListTable.getSelectionModel().addListSelectionListener(e -> {
+            int selectedRow = productListTable.getSelectedRow();
+            if (selectedRow >= 0) {
+                // Get the selected product details
+                String productId = tableModel.getValueAt(selectedRow, 0).toString();
+                String productName = tableModel.getValueAt(selectedRow, 1).toString();
+                String category = tableModel.getValueAt(selectedRow, 2).toString();
+                String price = tableModel.getValueAt(selectedRow, 3).toString();
+                String numOfItems = tableModel.getValueAt(selectedRow, 4).toString();
+                String info = tableModel.getValueAt(selectedRow, 5).toString();
+
+                // Display the product details in the details panel
+                displayProductDetails(productId, productName, category, price, numOfItems, info);
+            }
+        });
 
 
         // Populate the table with data from the ArrayList
         ArrayList<Clothing> clothList = manager.getClothList();
         for (Clothing clothing : clothList) {
-            Object[] rowData = {clothing.getProductId(), clothing.getProductName(),clothing.getCategory(), clothing.getPrice(),clothing.getInfo()};
+            Object[] rowData = {clothing.getProductId(), clothing.getProductName(),clothing.getCategory(), clothing.getPrice(),clothing.getNumOfItems(),clothing.getInfo()};
             tableModel.addRow(rowData);
         }
         ArrayList<Electronics> electList = manager.getElectList();
         for (Electronics electronics : electList) {
-            Object[] rowData = {electronics.getProductId(), electronics.getProductName(),electronics.getCategory(), electronics.getPrice(),electronics.getInfo()};
+            Object[] rowData = {electronics.getProductId(), electronics.getProductName(),electronics.getCategory(), electronics.getPrice(),electronics.getNumOfItems(),electronics.getInfo()};
             tableModel.addRow(rowData);
         }
         productListTable.setDefaultEditor(Object.class, null); // get an uneditable table
@@ -95,29 +120,12 @@ public class ShoppingGUI extends JFrame {
     }
 
     // Method to sort the table alphabetically based on product name
-    private void sortTable() {
-        ArrayList<Object[]> data = new ArrayList<>();
-        for (int i = 0; i < tableModel.getRowCount(); i++) {
-            Object[] rowData = new Object[tableModel.getColumnCount()];
-            for (int j = 0; j < tableModel.getColumnCount(); j++) {
-                rowData[j] = tableModel.getValueAt(i, j);
-            }
-            data.add(rowData);
-        }
 
-        data.sort(Comparator.comparing(o -> o[1].toString())); // Sorting based on product name
-
-        tableModel.setRowCount(0); // Clear the table
-
-        for (Object[] rowData : data) {
-            tableModel.addRow(rowData);
-        }
-    }
     // Method to update the table based on the selected category
     private void updateTable(String selectedCategory, DefaultTableModel tableModel, WestminsterShoppingManager manager) {
         // Check if tableModel is null, and initialize it if needed
         if (tableModel == null) {
-            String[] columnNames = {"Product ID", "Name", "Category", "Price", "Info"};
+            String[] columnNames = {"Product ID", "Name", "Category", "Price", "Number of Items","Info"};
             tableModel = new DefaultTableModel(columnNames, 0);
         }
         // Clear the table
@@ -136,12 +144,62 @@ public class ShoppingGUI extends JFrame {
     // Method to populate the table with data from the ArrayLists
     private void populateTable(ArrayList<Clothing> clothList, ArrayList<Electronics> electList, DefaultTableModel tableModel) {
         for (Clothing clothing : clothList) {
-            Object[] rowData = {clothing.getProductId(), clothing.getProductName(), clothing.getCategory(), clothing.getPrice(), clothing.getInfo()};
+            Object[] rowData = {clothing.getProductId(), clothing.getProductName(), clothing.getCategory(), clothing.getPrice(),clothing.getNumOfItems(), clothing.getInfo()};
+            if (clothing.getNumOfItems() < 3) {
+                // Set the background color of the entire row to red
+                Arrays.fill(rowData, Color.RED);
+            }
             tableModel.addRow(rowData);
+
+
         }
         for (Electronics electronics : electList) {
-            Object[] rowData = {electronics.getProductId(), electronics.getProductName(), electronics.getCategory(), electronics.getPrice(), electronics.getInfo()};
+            Object[] rowData = {electronics.getProductId(), electronics.getProductName(), electronics.getCategory(), electronics.getPrice(),electronics.getNumOfItems(), electronics.getInfo()};
             tableModel.addRow(rowData);
         }
+    }
+    private void sortTable() {
+        ArrayList<Object[]> data = new ArrayList<>();
+        for (int i = 0; i < tableModel.getRowCount(); i++) {
+            Object[] rowData = new Object[tableModel.getColumnCount()];
+            for (int j = 0; j < tableModel.getColumnCount(); j++) {
+                rowData[j] = tableModel.getValueAt(i, j);
+            }
+            data.add(rowData);
+        }
+        data.sort(Comparator.comparing(o -> o[1].toString())); // Sorting based on product name
+
+        tableModel.setRowCount(0); // Clear the table
+
+        for (Object[] rowData : data) {
+            tableModel.addRow(rowData);
+        }
+    }
+    // Add a method to display product details in the details panel
+    private void displayProductDetails(String productId, String productName, String category, String price, String numOfItems, String info) {
+        // Clear the existing content in the details panel
+        productDetailsPanel.removeAll();
+
+        // Create labels to display product details
+        JLabel productIdLabel = new JLabel("Product ID: " + productId);
+        JLabel productNameLabel = new JLabel("Name: " + productName);
+        JLabel categoryLabel = new JLabel("Category: " + category);
+        JLabel priceLabel = new JLabel("Price: " + price);
+        JLabel numOfItemsLabel = new JLabel("Number of Items: " + numOfItems);
+        JLabel infoLabel = new JLabel("Info: " + info);
+
+        // Add labels to the details panel
+        productDetailsPanel.add(productIdLabel);
+        productDetailsPanel.add(productNameLabel);
+        productDetailsPanel.add(categoryLabel);
+        productDetailsPanel.add(priceLabel);
+        productDetailsPanel.add(numOfItemsLabel);
+        productDetailsPanel.add(infoLabel);
+
+        // Repaint the details panel
+        productDetailsPanel.revalidate();
+        productDetailsPanel.repaint();
+
+
     }
 }
--- Online_Shopping_Center/src/Test.java
+++ Online_Shopping_Center/src/Test.java
@@ -2,8 +2,8 @@ public class Test {
     public static void main(String[] args) {
         ShoppingManager manager = new WestminsterShoppingManager();
         Electronics e1 = new Electronics("w1","dphone",10,100,"apple",20);
-        Electronics e2 = new Electronics("w2","atab",15,100,"samsung",20);
-        Clothing c1 = new Clothing("q1","cshirt",5,30,"small","black");
+        Electronics e2 = new Electronics("w2","atab",1,100,"samsung",20);
+        Clothing c1 = new Clothing("q1","cshirt",2,30,"small","black");
         Clothing c2 = new Clothing("q2","bshort",15,34,"xl","yellow");
         manager.addProduct(e1);
         manager.addProduct(e2);
